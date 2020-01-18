e5a1c625bce2a4f2cc4b2fc74d77977676755ae8
Yohel Domingo Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2020-01-17 22:32:39 -0300

    Antes de Instalar o MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 //app tem os seguintes metodos (http): get (obter), post (enviar), put (editar), delete (deletar)
 //tipos de parâmetros
-//query params: req.query(filtros, ordenação, paginação, ...)
-//route params: request.params ()
-//body
+//query params: request.query(filtros, ordenação, paginação, ...)
+//route params: request.params (identificar um recurso na alteração ou remoção)
+//body: request.body(dados para criação ou alteração de um registro)
+
+//MongoDB (Banco não relacional)
+
 app.post('/users', (request,response)=> {
-    console.log(request.query);
+    console.log(request.body);
     return response.json({
         message:'Hello Yohel'
     });
