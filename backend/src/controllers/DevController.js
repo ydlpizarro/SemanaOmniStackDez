a4e4e80ca3c9dae3c808b59f54f05c720817931a
Yohel Domingo Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2020-01-24 09:12:00 -0300

    Finalizando Backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 //index, show, store, update, destroy
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
         if (!dev) {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login, avatar_url, bio } = apiResponse.data;
-            const techsArray = techs.split(',').map(tech => tech.trim());
+            const techsArray = parseStringAsArray(techs);
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude],
--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,11 +1,27 @@
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
     async index(request, response){
-        console.log(request.query);
+        const {latitude, longitude, techs}=request.query;
+        const techsArray = parseStringAsArray(techs);
         //buscar todos os devs em um raio de 10 km
         //filtrar por tecnologia
-        return response.json({devs:[]});
+        const devs = await Dev.find({
+            techs:{
+                $in:techsArray,
+            },
+            location:{
+                $near:{
+                    $geometry:{
+                        type:'Point',
+                        coordinates:[longitude,latitude],
+                        $maxDistance:10000,
+                    }
+                }
+            },
+        });
+        return response.json({devs})
 
     }
 }
