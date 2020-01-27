97c4a6b41672a3a0971f71846337753bfcdcc29c
Yohel Domingo Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2020-01-27 15:03:24 -0300

    Melhorando o código

--- web/src/App.js
+++ web/src/App.js
@@ -52,7 +52,7 @@ function App() {
 
   useEffect(() => {
     async function loadDevs() {
-      const response = await api.get('./devs');
+      const response = await api.get('/devs');
 
       setDevs(response.data);
     }
@@ -107,16 +107,14 @@ function App() {
               <header>
                 <img src={dev.avatar_url} alt={dev.name} />
                 <div className="user-info">
-                  <span>{dev.techs.join(', ')}</span>
                   <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
                 </div>
               </header>
               <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Accese ao Github</a>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
             </li>
-
           ))}
-
         </ul>
       </main>
     </div>
